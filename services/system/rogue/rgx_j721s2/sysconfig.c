bb330491981f7878be92703ca85c05cf5e49052f
Randolph Sapp <rs@ti.com>
2023-04-04 17:42:47 -0500

    all: add j721s2_linux platform and makefiles
    System specific settings are configured via makefiles under the build
    directory. Adding one for J721S2 devices.
    
    These are the configs from the linuxws/kirkstone/k5.10/1.18.6276027
    branch as of: 3bcae895989fa187ba97ce437dbc948cec24d0a4
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

---
+++ services/system/rogue/rgx_j721s2/sysconfig.c
@@ -0,0 +1,290 @@
+/*************************************************************************/ /*!
+@File           sysconfig.c
+@Title          System Configuration
+@Copyright      Copyright (c) Imagination Technologies Ltd. All Rights Reserved
+@Description    Implements the system layer for TI Keystone3 SoCs
+@License        Dual MIT/GPLv2
+
+The contents of this file are subject to the MIT license as set out below.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+Alternatively, the contents of this file may be used under the terms of
+the GNU General Public License Version 2 ("GPL") in which case the provisions
+of GPL are applicable instead of those above.
+
+If you wish to allow use of your version of this file only under the terms of
+GPL, and not to allow others to use your version of this file under the terms
+of the MIT license, indicate your decision by deleting the provisions above
+and replace them with the notice and other provisions required by GPL as set
+out in the file called "GPL-COPYING" included in this distribution. If you do
+not delete the provisions above, a recipient may use your version of this file
+under the terms of either the MIT license or GPL.
+
+This License is also included in this distribution in the file called
+"MIT-COPYING".
+
+EXCEPT AS OTHERWISE STATED IN A NEGOTIATED AGREEMENT: (A) THE SOFTWARE IS
+PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE AND NONINFRINGEMENT; AND (B) IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/ /**************************************************************************/
+
+#include <linux/dma-mapping.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/pm.h>
+#include <linux/pm_domain.h>
+#include <linux/pm_runtime.h>
+
+#include "img_defs.h"
+#include "physheap.h"
+#include "pvrsrv.h"
+#include "rgxdevice.h"
+#include "interrupt_support.h"
+
+#define SYS_RGX_ACTIVE_POWER_LATENCY_MS 100
+#define RGX_HW_CORE_CLOCK_SPEED (800 * 1000 * 1000)
+
+/* Setup RGX specific timing data */
+static RGX_TIMING_INFORMATION gsRGXTimingInfo = {
+	.ui32CoreClockSpeed = RGX_HW_CORE_CLOCK_SPEED,
+	.bEnableActivePM = IMG_FALSE,
+	.ui32ActivePMLatencyms = SYS_RGX_ACTIVE_POWER_LATENCY_MS,
+	.bEnableRDPowIsland = IMG_FALSE,
+};
+
+static RGX_DATA gsRGXData = {
+	.psRGXTimingInfo = &gsRGXTimingInfo,
+};
+
+static PVRSRV_DEVICE_CONFIG	gsDevice;
+
+/*
+	CPU to Device physical address translation
+*/
+static
+void UMAPhysHeapCpuPAddrToDevPAddr(IMG_HANDLE hPrivData,
+								   IMG_UINT32 ui32NumOfAddr,
+								   IMG_DEV_PHYADDR *psDevPAddr,
+								   IMG_CPU_PHYADDR *psCpuPAddr)
+{
+	PVR_UNREFERENCED_PARAMETER(hPrivData);
+
+	/* Optimise common case */
+	psDevPAddr[0].uiAddr = psCpuPAddr[0].uiAddr;
+	if (ui32NumOfAddr > 1)
+	{
+		IMG_UINT32 ui32Idx;
+		for (ui32Idx = 1; ui32Idx < ui32NumOfAddr; ++ui32Idx)
+		{
+			psDevPAddr[ui32Idx].uiAddr = psCpuPAddr[ui32Idx].uiAddr;
+		}
+	}
+}
+
+/*
+	Device to CPU physical address translation
+*/
+static
+void UMAPhysHeapDevPAddrToCpuPAddr(IMG_HANDLE hPrivData,
+								   IMG_UINT32 ui32NumOfAddr,
+								   IMG_CPU_PHYADDR *psCpuPAddr,
+								   IMG_DEV_PHYADDR *psDevPAddr)
+{
+	PVR_UNREFERENCED_PARAMETER(hPrivData);
+
+	/* Optimise common case */
+	psCpuPAddr[0].uiAddr = psDevPAddr[0].uiAddr;
+	if (ui32NumOfAddr > 1)
+	{
+		IMG_UINT32 ui32Idx;
+		for (ui32Idx = 1; ui32Idx < ui32NumOfAddr; ++ui32Idx)
+		{
+			psCpuPAddr[ui32Idx].uiAddr = psDevPAddr[ui32Idx].uiAddr;
+		}
+	}
+}
+
+static PHYS_HEAP_FUNCTIONS gsPhysHeapFuncs = {
+	.pfnCpuPAddrToDevPAddr = UMAPhysHeapCpuPAddrToDevPAddr,
+	.pfnDevPAddrToCpuPAddr = UMAPhysHeapDevPAddrToCpuPAddr,
+};
+
+static PHYS_HEAP_CONFIG gsPhysHeapConfig = {
+	.pszPDumpMemspaceName = "SYSMEM",
+	.eType = PHYS_HEAP_TYPE_UMA,
+	.psMemFuncs = &gsPhysHeapFuncs,
+	.hPrivData = NULL,
+	.ui32UsageFlags = PHYS_HEAP_USAGE_GPU_LOCAL,
+};
+
+static void SysDevPowerDomainsDeinit(struct device *dev)
+{
+	struct device_link *link;
+
+	list_for_each_entry(link, &dev->links.consumers, s_node)
+		device_link_del(link);
+}
+
+static int SysDevPowerDomainsInit(struct device *dev)
+{
+	int err;
+	struct device *gpu_0, *gpucore_0;
+	struct device_link *gpu_0_dl, *gpucore_0_dl;
+
+	gpu_0 = dev_pm_domain_attach_by_name(dev, "gpu_0");
+	if (IS_ERR(gpu_0))
+	{
+		err = PTR_ERR(gpu_0);
+		dev_err(dev, "failed to get gpu_0 pm-domain: %d\n", err);
+		return err;
+	}
+
+	gpucore_0 = dev_pm_domain_attach_by_name(dev, "gpucore_0");
+	if (IS_ERR(gpucore_0))
+	{
+		err = PTR_ERR(gpucore_0);
+		dev_err(dev, "failed to get gpucore_0 pm-domain: %d\n", err);
+		return err;
+	}
+
+	gpu_0_dl = device_link_add(dev, gpu_0,
+					       DL_FLAG_PM_RUNTIME |
+					       DL_FLAG_STATELESS |
+                           DL_FLAG_RPM_ACTIVE);
+	if (!gpu_0_dl)
+	{
+		dev_err(dev, "adding gpu_0 device link failed!\n");
+		return -ENODEV;
+	}
+
+	gpucore_0_dl = device_link_add(dev, gpucore_0,
+					     DL_FLAG_PM_RUNTIME |
+					     DL_FLAG_STATELESS |
+					     DL_FLAG_RPM_ACTIVE);
+	if (!gpucore_0_dl)
+	{
+		dev_err(dev, "adding gpucore_0 device link failed!\n");
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+PVRSRV_ERROR SysDevInit(void *pvOSDevice, PVRSRV_DEVICE_CONFIG **ppsDevConfig)
+{
+	struct platform_device *psDev;
+	struct resource *dev_res = NULL;
+	int dev_irq;
+
+	psDev = to_platform_device((struct device *)pvOSDevice);
+	PVR_LOG(("Device: %s", psDev->name));
+
+	dma_set_mask(pvOSDevice, DMA_BIT_MASK(40));
+
+	dev_irq = platform_get_irq(psDev, 0);
+	if (dev_irq < 0)
+	{
+		PVR_DPF((PVR_DBG_ERROR, "%s: platform_get_irq failed (%d)", __func__, -dev_irq));
+		return PVRSRV_ERROR_INVALID_DEVICE;
+	}
+
+	dev_res = platform_get_resource(psDev, IORESOURCE_MEM, 0);
+	if (dev_res == NULL)
+	{
+		PVR_DPF((PVR_DBG_ERROR, "%s: platform_get_resource failed", __func__));
+		return PVRSRV_ERROR_INVALID_DEVICE;
+	}
+
+	/* Make sure everything we don't care about is set to 0 */
+	memset(&gsDevice, 0, sizeof(gsDevice));
+
+	/* Setup the device config */
+	gsDevice.pvOSDevice                         = pvOSDevice;
+	gsDevice.pszName                            = "rgx_j721s2";
+	gsDevice.pszVersion                         = NULL;
+
+	/* Device setup information */
+	gsDevice.sRegsCpuPBase.uiAddr    = dev_res->start;
+	gsDevice.ui32RegsSize            = (unsigned int)(dev_res->end - dev_res->start);
+	gsDevice.ui32IRQ                 = dev_irq;
+
+	/* Device's physical heaps */
+	gsDevice.pasPhysHeaps      = &gsPhysHeapConfig;
+	gsDevice.ui32PhysHeapCount = 1;
+	gsDevice.eDefaultHeap      = PVRSRV_PHYS_HEAP_GPU_LOCAL;
+
+	/* Setup RGX specific timing data */
+	gsDevice.hDevData = &gsRGXData;
+
+	gsDevice.bHasFBCDCVersion31      = IMG_FALSE;
+	gsDevice.bDevicePA0IsValid       = IMG_FALSE;
+
+	/* clock frequency */
+	gsDevice.pfnClockFreqGet        = NULL;
+
+	/* Set gsDevice.pfnSysDevErrorNotify callback */
+	gsDevice.pfnSysDevErrorNotify = SysRGXErrorNotify;
+
+	*ppsDevConfig = &gsDevice;
+
+	SysDevPowerDomainsInit(&psDev->dev);
+	pm_runtime_enable(&psDev->dev);
+	if (pm_runtime_get_sync(&psDev->dev) < 0)
+	{
+		PVR_LOG(("%s: failed to enable clock\n", __func__));
+	}
+
+	return PVRSRV_OK;
+}
+
+void SysDevDeInit(PVRSRV_DEVICE_CONFIG *psDevConfig)
+{
+	struct platform_device *psDev;
+	psDev = to_platform_device((struct device *)psDevConfig->pvOSDevice);
+	SysDevPowerDomainsDeinit(&psDev->dev);
+	psDevConfig->pvOSDevice = NULL;
+}
+
+PVRSRV_ERROR SysInstallDeviceLISR(IMG_HANDLE hSysData,
+								  IMG_UINT32 ui32IRQ,
+								  const IMG_CHAR *pszName,
+								  PFN_LISR pfnLISR,
+								  void *pvData,
+								  IMG_HANDLE *phLISRData)
+{
+	PVR_UNREFERENCED_PARAMETER(hSysData);
+	return OSInstallSystemLISR(phLISRData, ui32IRQ, pszName, pfnLISR, pvData,
+			SYS_IRQ_FLAG_TRIGGER_DEFAULT);
+}
+
+PVRSRV_ERROR SysUninstallDeviceLISR(IMG_HANDLE hLISRData)
+{
+	return OSUninstallSystemLISR(hLISRData);
+}
+
+PVRSRV_ERROR SysDebugInfo(PVRSRV_DEVICE_CONFIG *psDevConfig,
+				DUMPDEBUG_PRINTF_FUNC *pfnDumpDebugPrintf,
+				void *pvDumpDebugFile)
+{
+	PVR_UNREFERENCED_PARAMETER(psDevConfig);
+	PVR_UNREFERENCED_PARAMETER(pfnDumpDebugPrintf);
+	PVR_UNREFERENCED_PARAMETER(pvDumpDebugFile);
+	return PVRSRV_OK;
+}
+
+/******************************************************************************
+ End of file (sysconfig.c)
+******************************************************************************/
